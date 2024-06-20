943ac243811aa76297b7787e735006fc337ac9f0
Jovan Gómez <111164889+g0carDev@users.noreply.github.com>
2024-06-20 10:35:52 -0600

    create Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <p>Created by <a
+                    className="link-g0c4r"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.g0car.dev/">G0C4R</a> | 2024</p>
+            </div>
+        </footer>
+    )
+}
