63b5b499bfac9773d9bae1eda81a000bb38ff871
Jovan Gómez <111164889+g0carDev@users.noreply.github.com>
2024-06-20 10:36:57 -0600

    add components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { Footer, Navbar } from "@/components";
 import "./globals.css";
 
-const poppins = Poppins({weight: ['400', '500'], subsets: ["latin"]});
+const poppins = Poppins({ weight: ['400', '500'], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Challenge React App",
-  description: "Challenge React App",
+  title: "React Challenge App",
+  description: "React Challenge Created by G0C4R",
 };
 
 export default function RootLayout({
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
