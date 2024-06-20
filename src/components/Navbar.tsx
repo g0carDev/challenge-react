d16d04a0e4f13f81e9b7427eb6423facd5faaae4
Jovan Gómez <111164889+g0carDev@users.noreply.github.com>
2024-06-20 10:37:06 -0600

    create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <span>GOCAR <sub>React Challenge</sub></span>
+            </div>
+        </nav>
+    )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
-export { SearchInput } from './SearchInput';
-export { TableBank } from './TableBank' 
-export { TableActions } from './TableActions'
-export { TableBody } from './TableBody'
 export { Header } from './Header'
+export { Footer } from './Footer'
+export { Navbar } from './Navbar'
+export { TableBank } from './TableBank' 
+export { TableBody } from './TableBody'
+export { SearchInput } from './SearchInput';
+export { TableActions } from './TableActions'
 export { ImageMessage } from './ImageMessage'
